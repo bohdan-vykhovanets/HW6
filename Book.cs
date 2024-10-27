dbc3543ee6e2f4e699c136a421d10d44d9bd7fea
bohdan-vykhovanets <bohdan.vykhovanets@gmail.com>
2024-10-27 05:29:47 +0200

    completed

---
+++ Book.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json.Serialization;
+
+public class Book
+{
+    //[JsonIgnore] -- при додаванні цього рядка серіалізатор буде ігнорувати властивість
+    public int PublishingHouseId { get; set; }
+
+    //[JsonPropertyName("Name")] -- при додаванні цього рядка серіалізатор буде вважати, що ця властивість має назву "Name"
+    public string Title { get; set; }
+
+    public PublishingHouse PublishingHouse { get; set; }
+
+    public Book(int publishingHouseId, string title, PublishingHouse publishingHouse)
+    {
+        PublishingHouseId = publishingHouseId;
+        Title = title;
+        PublishingHouse = publishingHouse;
+    }
+}
+
+public class PublishingHouse
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Address { get; set; }
+
+    public PublishingHouse(int id, string name, string address)
+    {
+        Id = id;
+        Name = name;
+        Address = address;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System.Text.Json;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        string path = "./file.json";
+
+        string json = await File.ReadAllTextAsync(path);
+
+        List<Book> books;
+
+        if (json == null)
+        {
+            Console.WriteLine("Error: file is empty.");
+            return;
+        }
+        else
+        {
+            books = JsonSerializer.Deserialize<List<Book>>(json);
+        }
+                
+        foreach (var book in books)
+        {
+            Console.WriteLine($"Title: {book.PublishingHouseId}, Publisher: {book.PublishingHouse.Name}");
+        }
+    }
+}
